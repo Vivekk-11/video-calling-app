40b668e083af2e54807ba8136f6aa6cc1f8f02a3
vivek <chimnanivivek14@gmail.com>
2024-11-27 21:49:49 +0530

    signaling server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,5 +8,34 @@ let receiverSocket: null | WebSocket = null;
 wss.on("connection", (ws) => {
   ws.on("message", (data: any) => {
     const message = JSON.parse(data);
+    if (message.type === "sender") {
+      senderSocket = ws;
+    } else if (message.type === "receiver") {
+      receiverSocket = ws;
+    } else if (message.type === "create-offer") {
+      if (ws !== senderSocket) return;
+      receiverSocket?.send(
+        JSON.stringify(
+          JSON.stringify({ type: "createOffer", sdp: message.sdp })
+        )
+      );
+    } else if (message.type === "create-answer") {
+      if (ws !== receiverSocket) return;
+      senderSocket?.send(
+        JSON.stringify(
+          JSON.stringify({ type: "createAnswer", sdp: message.sdp })
+        )
+      );
+    } else if (message.type === "iceCandidate") {
+      if (ws === senderSocket) {
+        receiverSocket?.send(
+          JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+        );
+      } else {
+        senderSocket?.send(
+          JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+        );
+      }
+    }
   });
 });
