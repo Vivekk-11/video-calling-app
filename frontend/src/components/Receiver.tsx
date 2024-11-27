bcf9879af727ea10f32550a1f303415fc38c16d4
vivek <chimnanivivek14@gmail.com>
2024-11-27 22:08:00 +0530

    sender code on frontend

--- frontend/src/components/Receiver.tsx
+++ frontend/src/components/Receiver.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 const Receiver = () => {
+  useEffect(() => {
+    const socket = new WebSocket("http://localhost:8000");
+    socket.onopen = () => {
+      socket.send(JSON.stringify({ type: "receiver" }));
+    };
+  }, []);
+
   return <div>receiver</div>;
 };
 
--- frontend/src/components/Sender.tsx
+++ frontend/src/components/Sender.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
 const Sender = () => {
-  return <div>Sender</div>;
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket("http://localhost:8000/");
+    socket.onopen = () => {
+      socket.send(JSON.stringify({ type: "sender" }));
+    };
+    setSocket(socket);
+  }, []);
+
+  const startSendingVideo = async () => {
+    const pc = new RTCPeerConnection();
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+    socket?.send(
+      JSON.stringify({ type: "createOffer", sdp: pc.localDescription })
+    );
+  };
+
+  return (
+    <div>
+      Sender
+      <button onClick={startSendingVideo}>Send Video</button>
+    </div>
+  );
 };
 
 export default Sender;
